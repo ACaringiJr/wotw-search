cbaa2bcbf80fbce503ccc0b534b2cfa1d63f8e98
ACaringiJr <acaringijr@gmail.com>
2025-01-08 20:36:06 -0500

    edit script file to find link from github

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 document.querySelector("form.sectionForm").addEventListener("submit", function(event) {
   event.preventDefault();
-  
+
   let iframe = document.querySelector(".strayer");
   let section = document.getElementById("section").value;
 
-  // Set the iframe source URL
-  iframe.src = "https://digfir-published.macmillanusa.com/strayersources3ehs/strayersources3ehs_ch" + section + ".html";
-  
-  document.querySelector("main").style.width  = iframe.contentWindow.document.body.scrollWidth + 'px';
+  // Set the iframe source URL to the local file
+  iframe.src = "./strayersources3ehs/strayersources3ehs_ch" + section + ".html";
+
+  // Resize the iframe based on the loaded content (only works if same-origin)
+  iframe.onload = function() {
+    iframe.style.width = iframe.contentWindow.document.body.scrollWidth + 'px';
+  };
 });
