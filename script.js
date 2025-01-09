eca4867b95e081c1044e5d697fcb9dcfde4f78b4
ACaringiJr <acaringijr@gmail.com>
2025-01-08 21:17:34 -0500

    center display

--- script.js
+++ script.js
@@ -4,11 +4,7 @@ document.querySelector("form.sectionForm").addEventListener("submit", function(e
   let iframe = document.querySelector(".strayer");
   let section = document.getElementById("section").value;
 
-  // Set the iframe source URL to the local file
   iframe.src = "./strayersources3ehs/strayersources3ehs_ch" + section + ".html";
 
-  // Resize the iframe based on the loaded content (only works if same-origin)
-  iframe.onload = function() {
-    iframe.style.width = iframe.contentWindow.document.body.scrollWidth + 'px';
-  };
+  document.querySelector("main").style.width  = iframe.contentWindow.document.body.scrollWidth + 'px';
 });
